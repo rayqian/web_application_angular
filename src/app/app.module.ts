032b2e0f71a0b6182281bcedb5ef829e0c7bb400
ray_qian <ray.qian@hotmail.com>
2020-11-03 20:21:47 -0600

    seperate the about page to a new component. use router and menu for navigation of existing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ClientInfoComponent } from './client-info/client-info.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'client-info', component:ClientInfoComponent},
+  {path: 'about', component:AboutComponent},
+  {path: '', redirectTo: '/client-info', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientInfoComponent
+    ClientInfoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
